dd5a160226cb61e6e24863b260952b4f0595320d
coppl0010 <coppl0010@gmail.com>
2024-04-28 16:56:35 -0400

    bugfixed speed typing

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -1,7 +1,11 @@
+window.onload = function() {
+document.getElementById("userInput").readOnly = true;
+  };
 let testText = "The quick brown fox jumps over the lazy dog.";
 let startTime, endTime;
 function startTest() {
     // Set the test text
+    document.getElementById("userInput").readOnly = false;
     document.getElementById("inputText").value = testText;
     
     // Reset results and timer
@@ -33,14 +37,20 @@ function startTest() {
                 if (timeElapsed !== 0 && !isNaN(typedWords)) {
                     wpm = Math.round((typedWords / timeElapsed) * 60);
                 }
+
+                var totalLength = userTypedText.length
     
                 // Display the results
                 var outputDiv = document.getElementById("output");
                 outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                    "<p>Total Length: " + totalLength + "</p>" +
                     "<p>Words Typed: " + typedWords + "</p>" +
                     "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                     "<p>Words Per Minute (WPM): " + wpm + "</p>";
-    
+                
+                // Reset typed text
+                var resetText = document.getElementById('userInput');
+                resetText.value = '';
                 // Reset the button
                 var button = document.getElementById("btn");
                 button.innerHTML = "Start Test";
